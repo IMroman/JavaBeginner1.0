a0a568462302ea7d2c39ad1028be593b15a21a0b
IMroman <Ronobi@yandex.ru>
2021-06-18 13:09:53 +0300

    Create HomeWork2.java
    1. Создайте переменную типа с плавающей точкой с наибольшим диапазоном чисел. Присвойте ей произвольное дробное значение.
    2. Создайте переменную целочисленного типа с наименьшим диапазоном чисел. Присвойте  ей наибольшее возможное значение.

---
+++ JavaCourses/src/HomeWork2.java
@@ -0,0 +1,7 @@
+public class HomeWork2 {
+    public static void main(String[] args) {
+        double a = 15.984564;
+        byte b = 127;
+        System.out.println (a + " - Переменная с плавающей точкой. " + b + " - целочисленная переменая с наименьшим диапазоном чисел и наибольшим возможным значением");
+    }
+}
