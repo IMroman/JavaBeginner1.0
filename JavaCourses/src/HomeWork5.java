c85b16c4992def3ff573ff2b2001d86f7abbf675
IMroman <Ronobi@yandex.ru>
2021-06-19 23:04:57 +0300

    Create HomeWork5.java
    Напишите простой калькулятор:
    
    1. Ввести 2 числа с клавиатуры, затем один из символов ‘+’, ‘-’, ‘*’ или ‘/’.
    2. В зависимости от символа вывести в консоль результат выполнения соответствующей арифметической операции.

---
+++ JavaCourses/src/HomeWork5.java
@@ -0,0 +1,70 @@
+import java.util.Scanner;
+
+public class HomeWork5 {
+    static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int num1 = getInt1();//считывает с консоли число и возвращает его
+        int num2 = getInt2();
+        char operation = getOperation();//считает с консоли операцию
+        int result = calc(num1,num2,operation);//результат вычисления
+        System.out.println("Результат операции: "+ result);
+    }
+
+    public static int getInt1(){
+        System.out.println("Введите первое число:");
+        int num1;
+        if(scanner.hasNextInt()){
+            num1 = scanner.nextInt();
+        } else {
+            System.out.println("Вы допустили ошибку при вводе числа. Попробуйте еще раз.");
+            num1 = getInt1();
+        }
+        return num1;
+    }
+    public static int getInt2(){
+        System.out.println("Введите второе число:");
+        int num2;
+        if(scanner.hasNextInt()){
+            num2 = scanner.nextInt();
+        } else {
+            System.out.println("Вы допустили ошибку при вводе числа. Попробуйте еще раз.");
+            num2 = getInt1();
+        }
+        return num2;
+    }
+
+    public static char getOperation(){
+        System.out.println("Введите операцию:");
+        char operation;
+        if(scanner.hasNext()){
+            operation = scanner.next().charAt(0);
+        } else {
+            System.out.println("Вы допустили ошибку при вводе операции. Попробуйте еще раз.");
+            operation = getOperation();
+        }
+        return operation;
+    }
+
+    public static int calc(int num1, int num2, char operation){
+        int result;
+        switch (operation){
+            case '+':
+                result = num1+num2;
+                break;
+            case '-':
+                result = num1-num2;
+                break;
+            case '*':
+                result = num1*num2;
+                break;
+            case '/':
+                result = num1/num2;
+                break;
+            default:
+                System.out.println("Ошибка вычисления операции.");
+                result = calc(num1, num2, getOperation());//рекурсия
+        }
+        return result;
+    }
+}
