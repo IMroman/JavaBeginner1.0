a7341c6658ec8b0c0e8af5a432a8ffabe99125b1
IMroman <Ronobi@yandex.ru>
2021-06-24 18:35:43 +0300

    Create HomeWork8.java
    Напишите программу, где пользователь вводит любое целое положительное число n. А программа суммирует все нечетные числа от 1 до введенного пользователем числа n.

---
+++ JavaCourses/src/HomeWork8.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class HomeWork8 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите положительное целое число");
+        firstMethod();
+    }
+    public static int firstMethod(){
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int sum = 0;
+        if (n<=0){
+            System.out.println("Вы ввели не положительное целое число, повторите ввод целого положительного числа");
+            return firstMethod();
+        } else {
+            for (int i = 1;i<n;i++){
+                    if(!(i%2==0)){//поставить условие, при котором будет проходить проверка на соответсвие введения именно нечетного числа пользователем
+                        //System.out.println("Нечетное число " + i + " в пределе, до " + n);//нечетное число
+                        sum = sum+i;
+                }
+            }
+        }System.out.println("Сумма всех нечетных чисел, в пределе до введенного с клавиатуры числа " + n + ", равна = "+ sum);
+      return 0;
+    }
+}
