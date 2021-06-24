5711347a304d2ff24253b495cafcdb3b73435e39
IMroman <Ronobi@yandex.ru>
2021-06-24 21:28:04 +0300

    Create HomeWork9.java
    Напишите программу:
    
    Пользователь вводит размер массива и данные с клавиатуры в массив типа double.
    
    Посчитайте среднее арифметическое элементов массива.
    
    После этого произведите вывод массива на экран, где каждый элемент массива умножается на среднее арифметическое

---
+++ JavaCourses/src/HomeWork9.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class HomeWork9 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double avg = 0;
+        System.out.println("Введите размер массива");
+        int size = scanner.nextInt();
+        System.out.println("Введите данные массива");
+        double [] u = new double[size];
+        double[] array = new double[size];
+
+            for (int i = 0; i < size; i++) {
+                array[i] = scanner.nextDouble();//ввод данных массива типа double
+                //System.out.println(array[i]); //double array
+            }
+
+        //AVG
+            for (int d = 0; d<size;d++){
+                avg = avg + array[d];
+            }   avg = avg/size;
+                //System.out.println("Среднее арифметическое элементов массива = " + avg);
+
+        //array[i]*AVG
+            for (int i = 0; i<size; i++) {
+            u[i] = array[i] * avg;
+                System.out.println(i+ " элемент массива = "+ array[i]+" умножается на среднее арифметическое = "+avg+ ". Произведение равно = " + u[i]);
+        }
+    }
+}
