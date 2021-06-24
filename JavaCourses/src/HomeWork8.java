de3b06bdb31d724ce8e893d62f8a973434ebca08
IMroman <Ronobi@yandex.ru>
2021-06-24 20:06:21 +0300

    Update HomeWork8.java
    Удален лишняя переменная класса Scanner в main методе

--- JavaCourses/src/HomeWork8.java
+++ JavaCourses/src/HomeWork8.java
@@ -2,7 +2,6 @@ import java.util.Scanner;
 
 public class HomeWork8 {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
         System.out.println("Введите положительное целое число");
         firstMethod();
     }
