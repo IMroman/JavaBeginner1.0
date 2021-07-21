f8b4b92d77de84f8fecd01ce477af208aeebed8a
IMroman <Ronobi@yandex.ru>
2021-07-21 22:14:08 +0300

    Update FinalTask3

--- JavaCourses/src/FinalTask3.java
+++ JavaCourses/src/FinalTask3.java
@@ -2,8 +2,10 @@ import java.util.Scanner;
 
 public class FinalTask3 {
     public static void main(String[] args) {
-/*Напишите программу, где пользователь вводит сначала количество строк n, затем сами строки. Среди данных строк найти строку с максимальным
-количеством различных символов. Если таких строк будет много, то вывести первую.
+/*Напишите программу, где пользователь вводит сначала количество строк n, затем сами строки.
+Среди данных строк найти строку с максимальным
+количеством различных символов.
+Если таких строк будет много, то вывести первую.
 
 Пример для теста работы программы:
 
@@ -12,31 +14,40 @@ public class FinalTask3 {
 Строка 2: анализ
 Строка 3: 111111111111
 Ответ: привет*/
+
         Scanner scn = new Scanner(System.in);
-        System.out.println("Введите количество строк");
+        System.out.print("Введите количество строк: ");
         int n = scn.nextInt();
-        int max = 0;
-        int ind = 0;
-        System.out.println("Количестов строк: " + n);
-        String[] str = new String[n];//длинна массива == количеству строк
-        System.out.println("Введите строки");
-        str[0] = scn.nextLine();//первая строка
+        scn.nextLine();
+        int symbols = 0;
+        String words = "";
+
         for (int i = 0; i < n; i++) {
-            str[i] = scn.nextLine();
-            int p = i + 1;
-            System.out.println("Строка " + p + ": " + str[i]);
-        }//поиск максимальной строки
-        for (int i = 0; i < n; i++) {
-            if (max < str[i].length()) {
-                max = str[i].length();
-                ind = i;
-            } else if (max == str[i].length()) {
-                continue;
+            System.out.print("Строка " + (i + 1) + ": ");
+            String str = scn.nextLine();
+            int a = chars(str);
+            if (a > symbols) {
+                symbols = a;
+                words = str;
             }
         }
-        System.out.println(str[ind]);
+        System.out.println(words);
+    }
+
+    public static int chars(String p) {
+        boolean[] b = new boolean[Character.MAX_VALUE];
+        for (int i = 0; i < p.length(); i++) {
+            b[p.charAt(i)] = true;
+        }
+
+        int val2 = 0;
+        for (int i = 0; i < b.length; i++) {
+            if (b[i]) {
+                val2++;
+            }
+        }
+        return val2;
     }
 }
-
 
 
